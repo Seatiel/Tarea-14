df24db608dd3b00f20be8a6cbca268bca651d01a
seatiel ygnacio rosario <seatiel_2010@hotmail.com>
2017-02-22 23:43:35 -0400

    continuar

--- TareaDetallePeliculas/UI/Registros/rActores.Designer.cs
+++ TareaDetallePeliculas/UI/Registros/rActores.Designer.cs
@@ -28,19 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ActorIdtextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Buscarbutton = new System.Windows.Forms.Button();
+            this.Eliminarbutton = new System.Windows.Forms.Button();
+            this.Guardarbutton = new System.Windows.Forms.Button();
+            this.Nuevobutton = new System.Windows.Forms.Button();
+            this.ActortextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // ActorIdtextBox
+            // 
+            this.ActorIdtextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ActorIdtextBox.Location = new System.Drawing.Point(90, 22);
+            this.ActorIdtextBox.Name = "ActorIdtextBox";
+            this.ActorIdtextBox.Size = new System.Drawing.Size(63, 27);
+            this.ActorIdtextBox.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Actor Id:";
+            // 
+            // Buscarbutton
+            // 
+            this.Buscarbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Buscarbutton.Location = new System.Drawing.Point(176, 18);
+            this.Buscarbutton.Name = "Buscarbutton";
+            this.Buscarbutton.Size = new System.Drawing.Size(79, 31);
+            this.Buscarbutton.TabIndex = 13;
+            this.Buscarbutton.Text = "Buscar";
+            this.Buscarbutton.UseVisualStyleBackColor = true;
+            // 
+            // Eliminarbutton
+            // 
+            this.Eliminarbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Eliminarbutton.Location = new System.Drawing.Point(279, 127);
+            this.Eliminarbutton.Name = "Eliminarbutton";
+            this.Eliminarbutton.Size = new System.Drawing.Size(94, 31);
+            this.Eliminarbutton.TabIndex = 12;
+            this.Eliminarbutton.Text = "Eliminar";
+            this.Eliminarbutton.UseVisualStyleBackColor = true;
+            // 
+            // Guardarbutton
+            // 
+            this.Guardarbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Guardarbutton.Location = new System.Drawing.Point(145, 127);
+            this.Guardarbutton.Name = "Guardarbutton";
+            this.Guardarbutton.Size = new System.Drawing.Size(94, 31);
+            this.Guardarbutton.TabIndex = 11;
+            this.Guardarbutton.Text = "Guardar";
+            this.Guardarbutton.UseVisualStyleBackColor = true;
+            // 
+            // Nuevobutton
+            // 
+            this.Nuevobutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nuevobutton.Location = new System.Drawing.Point(12, 127);
+            this.Nuevobutton.Name = "Nuevobutton";
+            this.Nuevobutton.Size = new System.Drawing.Size(94, 31);
+            this.Nuevobutton.TabIndex = 10;
+            this.Nuevobutton.Text = "Nuevo";
+            this.Nuevobutton.UseVisualStyleBackColor = true;
+            // 
+            // ActortextBox
+            // 
+            this.ActortextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ActortextBox.Location = new System.Drawing.Point(89, 73);
+            this.ActortextBox.Name = "ActortextBox";
+            this.ActortextBox.Size = new System.Drawing.Size(284, 27);
+            this.ActortextBox.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Actor:";
             // 
             // rActores
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(603, 440);
+            this.ClientSize = new System.Drawing.Size(402, 191);
+            this.Controls.Add(this.ActorIdtextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Buscarbutton);
+            this.Controls.Add(this.Eliminarbutton);
+            this.Controls.Add(this.Guardarbutton);
+            this.Controls.Add(this.Nuevobutton);
+            this.Controls.Add(this.ActortextBox);
+            this.Controls.Add(this.label1);
             this.Name = "rActores";
             this.Text = "rActores";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox ActorIdtextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Buscarbutton;
+        private System.Windows.Forms.Button Eliminarbutton;
+        private System.Windows.Forms.Button Guardarbutton;
+        private System.Windows.Forms.Button Nuevobutton;
+        private System.Windows.Forms.TextBox ActortextBox;
+        private System.Windows.Forms.Label label1;
     }
 }
